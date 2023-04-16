b7e47efb028470bb3d89ba858449ee926d01fe3a
fluoz <hmunshi12@gmail.com>
2023-04-16 16:57:41 +0700

    using xmlhttp

--- main.js
+++ main.js
@@ -1,29 +1,68 @@
 const main = document.getElementById("main");
 
 const getPosts = async () => {
-  const res = await fetch("http://localhost/tubes-web/tubes-web/api/post.php");
-  const data = await res.json();
-  let postData = data.data;
+  // main.innerHTML = "<loading-spinner></loading-spinner>";
+  // const res = await fetch("http://localhost/tubes-web/tubes-web/api/post.php");
+  // const data = await res.json();
+  // main.innerHTML = "";
+  // let postData = data.data;
 
-  if (postData.length == 0) {
-    main.innerHTML = `<h3>Tidak ada post!</h3>`;
-  }
+  // if (postData.length == 0) {
+  //   main.innerHTML = `<h3>Tidak ada post!</h3>`;
+  //   return;
+  // }
 
-  for (let x of postData) {
-    let path = "./assets/user-black.png";
-    if (x.profile_picture) {
-      path = "./assets/uploads/" + x.profile_picture;
+  // for (let x of postData) {
+  //   let path = "./assets/user-black.png";
+  //   if (x.profile_picture) {
+  //     path = "./assets/uploads/" + x.profile_picture;
+  //   }
+
+  //   main.innerHTML += `
+  //       <user-post id="${x.id}"
+  //           user-logo="${path}"
+  //           img="./assets/uploads/${x.image_url}"
+  //           description="${x.caption}"
+  //           name="${x.username}"
+  //           ${x.liked ? "liked" : ""}
+  //       </user-post>`;
+  // }
+
+  let xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function () {
+    main.innerHTML = "<loading-spinner></loading-spinner>";
+    if (this.readyState == 4 && this.status == 200) {
+      main.innerHTML = "";
+      const data = JSON.parse(this.responseText);
+      if (data.length == 0) {
+        main.innerHTML = `<h3>Tidak ada post!</h3>`;
+        return;
+      }
+      for (let x of data.data) {
+        let path = "./assets/user-black.png";
+        if (x.profile_picture) {
+          path = "./assets/uploads/" + x.profile_picture;
+        }
+
+        main.innerHTML += `
+            <user-post id="${x.id}" 
+                user-logo="${path}" 
+                img="./assets/uploads/${x.image_url}" 
+                description="${x.caption}" 
+                name="${x.username}"
+                ${x.liked ? "liked" : ""}
+            </user-post>`;
+      }
     }
+  };
 
-    main.innerHTML += `
-        <user-post id="${x.id}" 
-            user-logo="${path}" 
-            img="./assets/uploads/${x.image_url}" 
-            description="${x.caption}" 
-            name="${x.username}"
-            ${x.liked ? "liked" : ""}
-        </user-post>`;
-  }
+  xmlhttp.open(
+    "GET",
+    "http://localhost/tubes-web/tubes-web/api/post.php",
+    true
+  );
+  xmlhttp.send();
 };
 
 const onLoadWeb = async () => {
